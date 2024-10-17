a437ea6b5114008b984d5b339e9f856abf292338
Rafael <rafael.aguiar91@gmail.com>
2024-10-17 11:19:10 -0300

    chore: confingure sass in tsdx project lib

---
+++ tsdx.config.js
@@ -0,0 +1,13 @@
+const postcss = require('rollup-plugin-postcss');
+module.exports = {
+    rollup(config, options) {
+        config.plugins.push(
+            postcss({
+                module: true,
+                inject: true,
+                extract: !!options.writeMeta,
+            })
+        );
+        return config
+    },
+}
